0eb41574e0ffa80d5a77dc6d03e65c2ef1c74955
bleudev <95937737+bleudev@users.noreply.github.com>
2025-03-22 13:11:42 +0300

    Chart + remove disk_usage

--- pages/chart.py
+++ pages/chart.py
@@ -1,4 +1,59 @@
+from sqlite3 import connect
 import flet as ft
+from matplotlib.figure import Figure
+
+from analys import analys, read_last
+from utils import get_uptime_str, smooth_resize
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+import flet as ft
+from flet.matplotlib_chart import MatplotlibChart
+
+matplotlib.use("svg")
 
 def page_chart(page: ft.Page):
-    page.add(ft.Text('График'))
+    smooth_resize(page, 500, 700)
+    
+    
+    def __get_plot() -> Figure:
+        analys()
+        ut = read_last()[0]
+        sut = ut - 120000
+        mut = ut - 60000
+        
+        with connect('data.db') as conn:
+            cur = conn.cursor()
+            cur.execute('select * from compinfo where uptime > ?', (sut,))
+            r = cur.fetchall()
+            cur.close()
+        
+        points = np.array(sorted([[i[0], i[1]] for i in r], key=lambda x: x[0]))
+
+        fig, axs = plt.subplots(1, 1)
+        
+        axs.plot(points[:, 0], points[:, 1])
+        axs.set_xlim(sut, ut)
+        axs.set_xlabel("время с момента включения компьютера")
+        axs.grid(True, color="white", linestyle="--", linewidth=0.5)
+        axs.tick_params(colors="white", which="both")
+        axs.xaxis.label.set_color("white")
+        axs.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: get_uptime_str(int(x))))
+        axs.yaxis.label.set_color("white")
+        axs.spines["bottom"].set_color("white")
+        axs.spines["left"].set_color("white")
+
+        fig.tight_layout()
+        return fig
+
+    chart = MatplotlibChart(__get_plot(), isolated=True, transparent=True, expand=True)
+    
+    def update_chart():
+        while page.navigation_bar.selected_index == 1:
+            chart.figure = __get_plot()
+            chart.update()
+
+    page.add(chart)
+    update_chart()
--- pages/now.py
+++ pages/now.py
@@ -1,5 +1,6 @@
 import flet as ft
-from analys import analys, read_all
+from analys import analys, read_all, read_last
+from utils import get_uptime_str, smooth_resize
 
 INFO_FStr = '''
 Время работы: `{uptime}`
@@ -11,8 +12,7 @@ INFO_FStr = '''
 Температура видеокарты: `{t_gpu}`\\
 Использование видеокарты: `{u_gpu}`
 
-Использоание ОЗУ: `{u_ram}`\\
-Использование диска: `{u_disk}`
+Использоание ОЗУ: `{u_ram}`
 '''
 
 def format(s, **kwargs):
@@ -21,52 +21,34 @@ def format(s, **kwargs):
     return s
 
 def format_data(data: tuple):
-    uptime, temp_cpu, crit_temp_cpu, temp_gpu, cpu_usage, gpu_usage, ram_usage, disk_usage = [round(i, 2) for i in data]
-    ut_ms = uptime % 1000
-    uptime //= 1000
-    ut_s = uptime % 60
-    uptime //= 60
-    ut_min = uptime % 60
-    uptime //= 60
-    ut_h = uptime % 24
-    uptime //= 24
-    ut_d = uptime
-    
-    uptime_l = [ut_d, ut_h, ut_min, ut_s]
-    uptime_l2 = [str(i) for i in uptime_l if i > 0]
-    uptime_str = ':'.join(uptime_l2)
-    
+    uptime, temp_cpu, crit_temp_cpu, temp_gpu, cpu_usage, gpu_usage, ram_usage = [round(i, 2) for i in data]
+    uptime_str = get_uptime_str(uptime)
+        
     return format(
         INFO_FStr, uptime=uptime_str,
         t_cpu=f'{temp_cpu}°C', t_gpu=f'{temp_gpu}°C' if temp_gpu != -1 else '❌',
         u_cpu=f'{cpu_usage}%', u_gpu=f'{gpu_usage}%' if gpu_usage != -1 else '❌',
-        u_ram=f'{ram_usage}%', u_disk=f'{disk_usage}%'
+        u_ram=f'{ram_usage}%'
     )
 
 def page_now(page: ft.Page):
-    page.window.height = 350
-    page.window.min_height = 445
-    page.window.width = 300
-    page.window.min_width = 370
-    
+    smooth_resize(page, 350, 300)
     if read_all():
-        txt = ft.Markdown(format_data(read_all()[-1]))
+        txt = ft.Markdown(format_data(read_last()))
     else:
         txt = ft.Markdown('Анализ')
     
     def update_txt():
-        if page.navigation_bar.selected_index == 0:
+        while page.navigation_bar.selected_index == 0:
             try:
                 analys()
                 
-                txt.value = format_data(read_all()[-1])
+                txt.value = format_data(read_last())
                 
                 txt.update()
                 update_txt()
             except AssertionError:
                 return
-        else:
-            return
     
     page.add(txt)
     
