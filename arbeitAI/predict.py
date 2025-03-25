3233d0e2a87964ec5f38955cb427026ad74084be
bleudev <aitiiigg1@gmail.com>
2025-03-25 12:06:52 +0300

    Windows app

--- arbeitAI/predict.py
+++ arbeitAI/predict.py
@@ -10,4 +10,4 @@ def predict(data: ndarray, ut: int) -> tuple[int, float, float, float]:
     future_usage_cpu = np.clip(predict_usage_cpu_if_not_gpu(data), a_min=0, a_max=100)
 
     future_usage_ram = np.clip(predict_ram_if_not_gpu(data), a_min=0, a_max=100)
-    return ut, float(future_temp_cpu[0]), float(future_usage_cpu[0, 0]), float(future_usage_ram[0, 0])
+    return ut, float(future_temp_cpu[0]), float(future_usage_cpu[0]), float(future_usage_ram[0])
--- cpu_temp.js
+++ cpu_temp.js
@@ -3,5 +3,5 @@ const fs = require('node:fs');
 
 si.cpuTemperature().then(data => {
     var content = String(data.main);
-    fs.writeFile('./cpu_temp.res', content, err => { if (err) console.error(err); });
+    fs.writeFile('./cpu_temp.res', content, 'utf8', err => { if (err) console.error(err); });
 }).catch(err => { console.error("Ошибка:", err); });
